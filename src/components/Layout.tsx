86824e9cfdb06444b6fb7ea70fd42e213a7e1af2
Larry the Cucumber <larrythecucumber321@gmail.com>
2023-02-03 19:16:18 -0800

    add identity script

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,6 +11,7 @@ export default function Layout({ children }: Props) {
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <Link rel="manifest" href="/site.webmanifest">
           asdf
         </Link>
